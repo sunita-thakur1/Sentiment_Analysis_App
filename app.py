a6251a0b49177418802710d5c5841a863cce7573
sunita-thakur1 <67176792+sunita-thakur1@users.noreply.github.com>
2025-01-03 21:47:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 # Function to perform sentiment analysis using VADER
 def analyze_sentiment_vader(review):
-    sentiment_score = sia.polarity_scores(review)
+    #sentiment_score = sia.polarity_scores(review)
     if sentiment_score['compound'] >= 0.05:
         return "Positive"
     elif sentiment_score['compound'] <= -0.05:
@@ -22,7 +22,7 @@ review = st.text_area("Enter the restaurant or other review here:")
 # If the user submits a review
 if st.button("Analyze Sentiment"):
     if review:
-        sentiment = analyze_sentiment_vader(review)
+        #sentiment = analyze_sentiment_vader(review)
         st.write(f"The sentiment of the review is: **{sentiment}**")
     else:
         st.warning("Please enter a review to analyze.")
